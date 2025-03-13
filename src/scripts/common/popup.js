d68f8b2907093b58c94539f734ebf47dae19d857
DrabantBY <yauheni.basalai@gmail.com>
2025-03-13 15:34:32 +0300

    feat(common): add burger menu

--- src/scripts/common/popup.js
+++ src/scripts/common/popup.js
@@ -1,15 +1,19 @@
-export default function togglePopup(element) {
-  const openPopup = document.getElementById(`${element}-open`);
-  const popup = document.getElementById(`${element}-popup`);
-  const closePopup = document.getElementById(`${element}-close`);
-  const overlay = document.getElementById('overlay');
+export function togglePopup(
+  popupSelector,
+  popupOpenSelector,
+  popupCloseSelector
+) {
+  var overlay = document.getElementById('overlay');
 
-  const onClick = () => {
-    popup.classList.toggle('visible');
-    overlay.classList.toggle('visible');
-    document.body.classList.toggle('static');
+  var popup = document.querySelector(popupSelector);
+  var openSelector = document.querySelector(popupOpenSelector);
+  var closeSelector = document.querySelector(popupCloseSelector);
+
+  var onClick = () => {
+    overlay.classList.toggle('display');
+    popup.classList.toggle('display');
   };
 
-  openPopup.addEventListener('click', onClick);
-  closePopup.addEventListener('click', onClick);
+  openSelector.addEventListener('click', onClick);
+  closeSelector.addEventListener('click', onClick);
 }
---
+++ src/scripts/index.js
@@ -0,0 +1,3 @@
+import { togglePopup } from '@scripts/common/popup';
+
+togglePopup('.burger-nav', '.burger-button', '.burger-nav-close');
